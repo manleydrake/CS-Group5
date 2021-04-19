c24b9c1be376c04a0e0fb3bdb28652392cea3a36
emmanixon520 <78748594+emmanixon520@users.noreply.github.com>
2021-04-19 10:36:55 -0500

    Add files via upload

--- Images/test2.py
+++ Images/test2.py
@@ -6,6 +6,7 @@ import pandas as pd
 df = pd.read_csv('moviesall.csv', dtype={"tconst": "string", "title": "string", "original_title": "string", 
     "year": int, "genre": "string", "duration": int, "country": "string", "language": "string", "director": "string", 
     "writer": "string", "description": "string", "avg_vote": float, "votes": int} )
+df = df.iloc[0:151]
 ids = []
 for i in df['tconst']:
     ids.append(i)
@@ -17,6 +18,8 @@ for i in ids:
 #print(urls)
 
 imgurls = []
+descrips = []
+primewatch = []
 
 for u in urls:    
     html_page = requests.get(u)
@@ -33,10 +36,17 @@ for u in urls:
     for item in soup.find_all('div', class_ = 'poster'):
         imgurls.append(item.img['src'])
 
-    #for item in soup.find_all('div', class_ = "inline canwrap"):
-        #descrips.append(item.span.text)
+    for item in soup.find_all('div', class_ = "inline canwrap"):
+        if item.span.text:
+            descrips.append(item.span.text)
+        else: 
+            descrips.append("NA")
+    for item in soup.find_all('div', class_ = "buybox buybox--default buybox--desktop"):
+        primewatch.append(item.a['href'])
 
 df['imageurls'] = imgurls
-print(df.head)
+df['fulldescriptions'] = descrips
+df['primewatch'] = primewatch
+#print(df.head)
 
-df.to_csv('export.csv', header=True)
+df.to_csv('fulldescprimelink150.csv', header=True)
