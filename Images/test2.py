aac65e320afa9dc5c744a62ff3a902d58ac465a1
emmanixon520 <78748594+emmanixon520@users.noreply.github.com>
2021-05-05 10:40:50 -0500

    Add files via upload
    updated test2 and all movies without NA

--- Images/test2.py
+++ Images/test2.py
@@ -6,7 +6,7 @@ import pandas as pd
 df = pd.read_csv('moviesall.csv', dtype={"tconst": "string", "title": "string", "original_title": "string", 
     "year": int, "genre": "string", "duration": int, "country": "string", "language": "string", "director": "string", 
     "writer": "string", "description": "string", "avg_vote": float, "votes": int} )
-df = df.iloc[0:151]
+df = df.iloc[4000:5000]
 ids = []
 for i in df['tconst']:
     ids.append(i)
@@ -19,34 +19,28 @@ for i in ids:
 
 imgurls = []
 descrips = []
-primewatch = []
+watchlink = []
 
 for u in urls:    
     html_page = requests.get(u)
     
     soup = BeautifulSoup(html_page.content, 'html.parser')
-
-    #for item in soup.find_all('h1'):
-        #title = item.text
-        #year = re.search('\(([^)]+)', title).group(1)
-        #title = title.split('(')[0]
-        #years.append(year)
-        #titles.append(titles)
         
     for item in soup.find_all('div', class_ = 'poster'):
         imgurls.append(item.img['src'])
 
-    for item in soup.find_all('div', class_ = "inline canwrap"):
-        if item.span.text:
-            descrips.append(item.span.text)
-        else: 
-            descrips.append("NA")
-    for item in soup.find_all('div', class_ = "buybox buybox--default buybox--desktop"):
-        primewatch.append(item.a['href'])
+    if soup.find('div', class_ = "inline canwrap"):
+        descrips.append(soup.find('div', class_ = "inline canwrap").span.text)
+    else:
+        descrips.append("NA")
 
+    if soup.find('div', class_ = "buybox buybox--default buybox--desktop"):
+        watchlink.append(soup.find('a', class_ = "tracked-offsite-link buybox__link")['href'])
+    else:
+        watchlink.append("NA")
+            
 df['imageurls'] = imgurls
 df['fulldescriptions'] = descrips
-df['primewatch'] = primewatch
-#print(df.head)
+df['linktowatch'] = watchlink
 
-df.to_csv('fulldescprimelink150.csv', header=True)
+df.to_csv('fulldescwatchlink40005000.csv', header=True)
